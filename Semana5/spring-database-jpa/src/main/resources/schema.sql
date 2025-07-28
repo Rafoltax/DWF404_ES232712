20a512376404913777658c10fa005d7ce0865760
Rafael Alejandro Escorcia Siliézar <rafaelitoescorcia@gmail.com>
2025-07-27 20:19:51 -0600

    Update schema.sql

--- Semana5/spring-database-jpa/src/main/resources/schema.sql
+++ Semana5/spring-database-jpa/src/main/resources/schema.sql
@@ -1,3 +1,27 @@
-drop table T_TEST if exists;
+--
+-- Tablas de la base de datos: `guia_jpa`
+--
 
-create table T_TEST (NAME varchar(50) not null);
+-- ----------------------------------------------------------------
+--  TABLE POST
+-- ----------------------------------------------------------------
+CREATE TABLE POST (
+    ID INT NOT NULL,
+    TITLE VARCHAR(1000) NOT NULL,
+	POST_DATE DATE NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+-- ----------------------------------------------------------------
+--  TABLE POST_COMMENT
+-- ----------------------------------------------------------------
+CREATE TABLE POST_COMMENT (
+    ID INT NOT NULL,
+    REVIEW VARCHAR(1000) DEFAULT NULL,
+	COMMENT_DATE DATE NOT NULL,
+    POST_ID INT DEFAULT NULL,
+    PRIMARY KEY(ID),
+    CONSTRAINT FK_POST_COMMENT_POST_ID
+        FOREIGN KEY (POST_ID)
+            REFERENCES POST(ID)
+);
