7ac73c7ff1bda07a3b34c62137fd28229ba654bb
Rafael Alejandro Escorcia Siliézar <rafaelitoescorcia@gmail.com>
2025-09-04 08:22:19 -0600

    Add files via upload

---
+++ Semana8/spring-api-rest/src/main/java/sv/edu/udb/repository/PostRepository.java
@@ -0,0 +1,52 @@
+package sv.edu.udb.repository;
+
+import sv.edu.udb.repository.domain.Post;
+import jakarta.transaction.Transactional;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+public class PostRepository {
+
+    private final SessionFactory sessionFactory;
+
+    public PostRepository(final SessionFactory sessionFactory) {
+        this.sessionFactory = Objects.requireNonNull(sessionFactory);
+    }
+
+    public List<Post> findAll() {
+        final String QUERY = "from Post p";
+        return sessionFactory
+                .getCurrentSession()
+                .createQuery(QUERY, Post.class)
+                .getResultList();
+    }
+
+    public Post findById(final Long id) {
+        return sessionFactory
+                .getCurrentSession()
+                .find(Post.class, id);
+    }
+
+    @Transactional
+    public void save(final Post post) {
+        sessionFactory.getCurrentSession().persist(post);
+    }
+
+    @Transactional
+    public void delete(final Post post) {
+        sessionFactory.getCurrentSession().remove(post);
+    }
+
+    @Transactional
+    public void deleteById(final Long id) {
+        final String QUERY = "delete from Post post where post.id=:id";
+        sessionFactory.getCurrentSession()
+                .createMutationQuery(QUERY)
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+}
